d43bc99df33981e8ef1f1754b338e0508fff7e39
kanki98 <karlo.kranjcec0307@gmail.com>
2025-03-15 16:47:16 +0100

    Fixed an issue with Edit view to Details view redirecting.

--- CRUD_Employees/Controllers/EmployeesController.cs
+++ CRUD_Employees/Controllers/EmployeesController.cs
@@ -193,7 +193,7 @@ namespace CRUD_Employees.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Details));
+                return RedirectToAction("Details", new { id = employee.Id });
             }
 
             ViewBag.Genders = EnumHelper.GetEnumSelectList<Gender>();
